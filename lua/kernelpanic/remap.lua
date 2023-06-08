5c1ec17dea7fb4ed55119fdef505bbd4a962133e
JannisK89 <jannis.karanikis@gmail.com>
2023-06-08 15:04:24 +0200

    added to keymap

--- lua/kernelpanic/remap.lua
+++ lua/kernelpanic/remap.lua
@@ -28,3 +28,9 @@ vim.keymap.set("n", "<leader>f", "<cmd> lua vim.lsp.buf.format()<CR>")
 
 -- Show Documentation
 vim.keymap.set("n", "<leader>d", "K")
+
+-- Open brackets
+vim.keymap.set("i", "<C-z>", "{<Enter><Enter>}<Escape>ka<Tab>")
+
+-- Open parentheses
+vim.keymap.set("i", "<C-x>", "()<Escape>i")
